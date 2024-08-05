ed60ac3be01e5683956e57d59d3ef49658a7b40f
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-08-05 11:17:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 /*  your css code here. If applicable */
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",() => {
 	document.body.innerText = "DOM load success";
 })
